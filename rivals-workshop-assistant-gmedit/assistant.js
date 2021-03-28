29b6a112c28cc449d951f5746b32d4846ce4a21d
Toren Darby <darby@science.ubc.ca>
2021-03-28 13:50:31 -0700

    Refactor

--- rivals-workshop-assistant-gmedit/assistant.js
+++ rivals-workshop-assistant-gmedit/assistant.js
@@ -1,14 +1,13 @@
 (function () {
     const childProcess = require("child_process")
     const pluginRootDir = $gmedit["plugins.PluginManager"].pluginDir['rivals-workshop-assistant-gmedit'];
-    const pluginDir = pluginRootDir + '/rivals-workshop-assistant-gmedit'
-    const projectDir = $gmedit["gml.Project"].current.dir;
-    const command = pluginDir
-        + `\\rivals_workshop_assistant.exe `
-        + projectDir;
-    console.log('Assistant command: ' + command);
+    const pluginDir = pluginRootDir + '/rivals-workshop-assistant-gmedit';
 
     function inject() {
+        const projectDir = $gmedit["gml.Project"].current.dir;
+        const command = pluginDir + `\\rivals_workshop_assistant.exe ` + projectDir;
+        console.log('Assistant command: ' + command);
+
         childProcess.execFile(command, [pluginDir], {shell: true}, (err, stdout, stderr) => {
             if (err) {
                 console.log(err);
